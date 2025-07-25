c65c718fb71ab1117c7d2dedc5cd445eb44b470f
SagarShimpi2000 <sagardshimpi88@gmail.com>
2025-07-25 11:27:36 +0530

    Quotation Api

---
+++ ZenithApp/Controllers/QuotationController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZenithApp.ZenithMessage;
+using ZenithApp.ZenithRepository;
+
+namespace ZenithApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class QuotationController : BaseController
+    {
+        private readonly QuotationRepository _quotationRepository;
+        private readonly IHttpContextAccessor _acc;
+
+        public QuotationController(QuotationRepository QuotationRepository, IHttpContextAccessor acc)
+        {
+            _quotationRepository = QuotationRepository;
+            _acc = acc;
+        }
+
+
+        [HttpPost("CreateQuotation")]
+        public IActionResult CreateQuotation(createQuotationRequest model)
+        {
+            var claims = HttpContext.User.Claims;
+            var userNameDetails = claims.FirstOrDefault(c => c.Type == "UserId");
+            var UserId = userNameDetails.Value;
+            _acc.HttpContext?.Session.SetString("UserId", UserId);
+            return this.ProcessRequest<createQuotationResponse>(model);
+        }
+
+       
+        protected override BaseResponse Execute(string action, BaseRequest request)
+        {
+            if (action == nameof(CreateQuotation))
+            {
+                return _quotationRepository.CreateQuotation(request as createQuotationRequest).Result;
+            }
+            
+
+            throw new NotImplementedException();
+        }
+        
+        protected override void Disposing()
+        {
+            this._quotationRepository.Dispose();
+        }
+    }
+}
