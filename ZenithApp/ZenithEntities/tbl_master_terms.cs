26742c4c61cfe551829011a0d3b02e783dedd591
SagarShimpi2000 <sagardshimpi88@gmail.com>
2025-08-25 10:53:52 +0530

    Updated changes upto 25-8-25

---
+++ ZenithApp/ZenithEntities/tbl_master_terms.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ZenithApp.ZenithEntities
+{
+    public class tbl_master_terms
+    {
+
+
+        [BsonId, BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string term { get; set; }
+
+        public bool isDelete { get; set; }
+      
+        public DateTime CreatedOn { get; set; }
+    }
+}
